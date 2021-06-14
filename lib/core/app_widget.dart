50b379df11cbe279c497424624e5f34957bd783c
Lucas Tamir Faria <lucastamir@gmail.com>
2021-06-14 18:27:49 -0300

    refactor: delete unused import

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,5 +1,4 @@
 import 'package:devquiz/home/home_page.dart';
-// import 'package:devquiz/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
